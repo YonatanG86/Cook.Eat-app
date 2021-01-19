4e156b749d4de69bcc05ca10e6a6febb933c31f4
Meandredelame <71453198+Meandredelame@users.noreply.github.com>
2021-01-19 18:26:05 +0200

    Navbar and login modal - merge with Isayas

--- cook.eat-front-end/src/Components/NavigationBar.js
+++ cook.eat-front-end/src/Components/NavigationBar.js
@@ -2,78 +2,83 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../styles/NavigationBar.css";
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
-// import { BiSearch } from 'react-icons/bi';
-// import cookEatLogo from '../img/'
+import { BiSearch } from 'react-icons/bi';
+import { FaHamburger } from 'react-icons/fa';
+import cookEatLogo from '../img/cookEatLogo.png'
 
 
 const NavigationBar = () => {
 
     return (
-        <Navbar className="main-navbar" expand="lg">
-        <Navbar.Brand>
-          <Link id="main-link" to="/">
-            <img
-            //   alt=""
-            //   src={cookEatLogo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            {" "}Cook.Eat
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="Browse?" id="basic-nav-dropdown">
-              <NavDropdown.Item 
-                as={Link} 
-                to="/my-feed" 
-                className="dropdown-link">
-                My Feed
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                as={Link}
-                to="/recipes"
-                className="dropdown-link">
-                Recipes
-              </NavDropdown.Item>
-              <NavDropdown.Divider id="dropdown-divider" />
-              <NavDropdown.Item
-                as={Link}
-                to="/something"
-                className="dropdown-link">
-                {" "}Something?
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link 
-              as={Link} 
-              to="/profile-settings" 
+        <div className="nav-container">
+          <Navbar className="main-navbar" expand="lg">
+            <Navbar.Brand>
+              <Link id="main-link" to="/">
+                {" "}
+                <span id="cook">Cook</span>
+                <span id="dot">.</span>
+                <span id="eat">Eat</span>
+                <img
+                  alt="logo"
+                  src={cookEatLogo}
+                  width="47"
+                  height="47"
+                  className="d-inline-block align-top"/>
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <NavDropdown title={<FaHamburger/>} id="basic-nav-dropdown">
+                  <NavDropdown.Item 
+                    as={Link} 
+                    to="/my-feed" 
+                    className="dropdown-link">
+                    My Feed
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/recipes"
+                    className="dropdown-link">
+                    Recipes
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider id="dropdown-divider" />
+                  <NavDropdown.Item
+                    as={Link}
+                    to="/something"
+                    className="dropdown-link">
+                    {" "}Something?
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link 
+                  as={Link} 
+                  to="/profile-settings" 
+                  id="links">
+                  Profile Settings
+                </Nav.Link>
+              </Nav>
+              <Form inline className="nav-search-bar">
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="search mr-sm-2"
+                />
+                <Button 
+                  id="nav-search-button" 
+                  variant="outline-success" 
+                  type="submit">
+                  <BiSearch />
+                </Button>
+              </Form>
+            </Navbar.Collapse>
+            <Nav.Link
+              as={Link}
+              to="/sign-out"
+              variant="outline-success"
               className="links">
-              Profile Settings
+              Sign Out
             </Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl
-              type="text"
-              placeholder="{BiSearch}"
-              className="mr-sm-2"
-            />
-            <Button 
-              className="nav-search-button" 
-              variant="outline-success" 
-              type="submit">
-              Search
-            </Button>
-          </Form>
-        </Navbar.Collapse>
-        <Nav.Link
-          as={Link}
-          to="/sign-out"
-          variant="outline-success"
-          className="logout-link">
-          Signup/Login <b>OR</b> Sign Out
-        </Nav.Link>
-      </Navbar>
+        </Navbar>
+      </div>
     )
 }
 
