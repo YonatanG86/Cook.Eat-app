6d1ab634d48243c94fe92b473cdbb6ba6554c1fd
Meandredelame <71453198+Meandredelame@users.noreply.github.com>
2021-01-24 13:18:01 +0200

    Profile settings WIP

--- cook.eat-front-end/src/Components/ProfileSettings.js
+++ cook.eat-front-end/src/Components/ProfileSettings.js
@@ -3,9 +3,10 @@ import '../styles/ProfileSettings.css';
 import { Col, Form, Button } from "react-bootstrap";
 import { CountryDropdown } from "react-country-region-selector";
 
+
 const ProfileSettings = () => {
-    const [email, setEmail] = useState(""); // This should already exist from the sign up
-    const [password, setPassword] = useState(""); // This should already exist from the sign up
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
     const [date, setDate] = useState("");
     const [country, setCountry] = useState("");
@@ -14,30 +15,8 @@ const ProfileSettings = () => {
     return (
         <div className="profile-settings-pagewrapper">
           <h4>Profile Settings</h4>
-          <Form onSubmit>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  className="form-input"
-                  name="email"
-                  type="email"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  className="form-input"
-                  name="password"
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange />
-              </Form.Group>
-            </Form.Row>
+          <Form className="profile-settings-form" onSubmit>
+            <h6>Edit Profile Settings</h6>
 
             <Form.Row>
               <Form.Group as={Col} controlId="formGridUsername">
@@ -50,53 +29,51 @@ const ProfileSettings = () => {
                   value={username}
                   onChange />
               </Form.Group>
+              <Form.Group as={Col} controlId="dob">
+                <Form.Label>Birth Date</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="dob"
+                  placeholder="Date of Birth"
+                  value={date}
+                  onChange />
+              </Form.Group>
             </Form.Row>
 
-            <div className="row-container">
-              <div className="row">
-                <div className="col-md-12">
-                  <Form.Group controlId="dob">
-                    <Form.Label>Birth Date</Form.Label>
-                    <Form.Control
-                      type="date"
-                      name="dob"
-                      placeholder="Date of Birth"
-                      value={date}
-                      onChange />
-                  </Form.Group>
-                </div>
-              </div>
-              <div className="country-select-container">
-                <CountryDropdown
-                  value={country}
-                  onChange >
-                </CountryDropdown>
-              </div>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  className="form-input"
+                  name="email"
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange />
+              </Form.Group>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className="form-input"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange />
+              </Form.Group>
+            </Form.Row>
 
-              {/* <div className="phone-input-container">
-                <Form.Group as={Col} md="12">
-                  <Form.Label>Phone Number</Form.Label>
-                  <PhoneInput
-                    type="phone-number"
-                    placeholder="Enter phone number"
-                    name="phone-number"
-                    value={toString(phone)}
-                    onChange={(phone) => setPhone({ phone })} />
-                </Form.Group>
-              </div> */}
+            <div className="country-select-container">
+              <CountryDropdown
+                value={country}
+                onChange >
+              </CountryDropdown>
             </div>
 
-            {/* <Form.Group className="textarea-container">
-              <Form.Label>Biography</Form.Label>
-              <Form.Control
-                className="form-input"
-                name="text"
-                placeholder="You can share your past extraterrestrial experience here."
-                as="textarea"
-                rows={3}
-                value={text}
-                onChange={(event) => handleTextChange(event)} />
-            </Form.Group> */}
+
+            <h6 id="culinary-preferences">Edit Culinary Preferences</h6>
+
+
 
             <Form.Group id="formGridCheckbox">
               <Form.Check
