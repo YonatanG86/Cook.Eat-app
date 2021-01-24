b89bd631b551d15ca5636d55413bda42d87354ab
yuval-mandler <yuvalmandler@gmail.com>
2021-01-24 12:14:52 +0200

    test1

--- cook.eat-front-end/src/Components/RecipeFullPage.jsx
+++ cook.eat-front-end/src/Components/RecipeFullPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 function RecipeFullPage() {
+
+
     return (
         <div>
-            
+            <h1>recipeTitle</h1>
+            <h3>Calories</h3>
+            <h3>Preparation Time</h3>
+            <h3>description</h3>
+            <img src="" alt="pasta" />
+            <h3>cuisine</h3>  
+            <h3>servings</h3>
+            <h3>dishLevel</h3>  
+            {steps && steps.map(step => {
+                <div>step</div>
+            })}    
         </div>
     )
 }
