43fcb49998d4c1d57eba32603d58eb34dcb25cb5
yuval-mandler <yuvalmandler@gmail.com>
2021-01-24 21:33:13 +0200

    recipePage

--- cook.eat-front-end/src/Components/RecipePage.jsx
+++ cook.eat-front-end/src/Components/RecipePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/recipePage.css'
-import { FaPen, FaHeart } from 'react-icons/fa'
+import { FaPen, FaHeart, FaFireAlt, FaClock } from 'react-icons/fa'
+import { GiCookingPot } from 'react-icons/gi'
 
 function RecipePage(props) {
 
@@ -27,29 +28,40 @@ function RecipePage(props) {
         <div>
             <h1 className='recipeTitle'>{recipe.recipeTitle}</h1>
             <h4 style={{fontSize: '14px'}}><FaPen style={{marginRight:'1rem'}}/>{recipe.writer}</h4> 
-            <div className='likeBox'> <FaHeart style={{marginRight:'1rem'}}/>{recipe.likes}</div>   
+            <div className='likeBox'> <FaHeart style={{marginRight:'1rem'}} />{recipe.likes}</div>   
+            <div className='desc'>{recipe.description}</div>
+            <div className='topic'>
+                <div className='cal'> <FaFireAlt style={{marginRight:'1rem'}}/>{recipe.calories} Cal.</div>
+                <div className='cal'> <GiCookingPot style={{marginRight:'1rem', fontSize: '30px'}}/>{recipe.dishLevel} </div>
+                <div className='cal clock'> <FaClock style={{marginRight:'1rem'}}/> {recipe.preparationTime} Min.</div>
+            </div>
         </div>
             <div className='imageBox'>
                 <img className='recipeImage' src={recipe.picture} alt="pasta" />
             </div>
         </div>
-            
-            <h3>{recipe.Calories}</h3>
-            <h3>{recipe.PreparationTime}</h3>
-            <h3>{recipe.description}</h3>
             <h3>{recipe.cuisineType}</h3>  
-            <h3>{recipe.servings}</h3>
-            <h3>{recipe.dishLevel}</h3>  
-            {recipe.steps.map(step => 
-                <div>1.{step}</div>
-            )}
-            {recipe.ingredients.map(ing => 
-                <div>
-                    <div>{ing.ingredientName}</div>
-                    <div>{ing.quantity}</div>
-                    <div>{ing.measurement}</div>
+            <div className='textBox'>
+                <div className='textLeft'>
+                    <h3>How to make it.</h3>
+                    <ol>
+                        {recipe.steps.map(step => 
+                            <li className='step'>{step}</li>
+                        )}
+                    </ol>  
                 </div>
-            )}
+                <div className='textRight'>
+                    <h3>Ingredients </h3>
+                    {recipe.servings && <h5>{recipe.servings} servings</h5>}
+                    {recipe.ingredients.map(ing => 
+                        <div className='ing'>
+                            <span>{ing.ingredientName}</span>
+                            <span>{ing.quantity}</span>
+                            <span>{ing.units}</span>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>: null}
         </div>
     )
