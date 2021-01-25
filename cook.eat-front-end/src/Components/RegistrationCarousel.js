e533f6c28a178e84ebb99e2e3187a2281f696284
Meandredelame <71453198+Meandredelame@users.noreply.github.com>
2021-01-25 17:10:15 +0200

    Profile settings - types of cuisines carousel finished

--- cook.eat-front-end/src/Components/RegistrationCarousel.js
+++ cook.eat-front-end/src/Components/RegistrationCarousel.js
@@ -1,70 +1,70 @@
-import React, { useState } from "react";
-import MultiFormSignUp from '../Components/MultiFormSignUp';
-import '../styles/RegistrationCarousel.css';
-import { Carousel } from "react-bootstrap";
+// import React, { useState } from "react";
+// import MultiFormSignUp from '../Components/MultiFormSignUp';
+// import '../styles/RegistrationCarousel.css';
+// import { Carousel } from "react-bootstrap";
 
-const RegistrationCarousel = () => {
-  const [index, setIndex] = useState(0);
+// const RegistrationCarousel = () => {
+//   const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex, event) => {
-      setIndex(selectedIndex);
-  };
+//   const handleSelect = (selectedIndex, event) => {
+//       setIndex(selectedIndex);
+//   };
 
-  const directionButtons = (direction) => {
-    return (
-      <span
-        aria-hidden="true"
-        className={direction === "Next" ? "button-next" : "button-prev"}>
-        {direction}
-      </span>
-    );
-  };
+//   const directionButtons = (direction) => {
+//     return (
+//       <span
+//         aria-hidden="true"
+//         className={direction === "Next" ? "button-next" : "button-prev"}>
+//         {direction}
+//       </span>
+//     );
+//   };
     
-    return (
-        <>
-          <Carousel
-            interval={null} 
-            activeIndex={index} 
-            nextIcon={directionButtons("Next")}
-            prevIcon={directionButtons("Previous")}
-            onSelect={handleSelect}>
+//     return (
+//         <>
+//           <Carousel
+//             interval={null} 
+//             activeIndex={index} 
+//             nextIcon={directionButtons("Next")}
+//             prevIcon={directionButtons("Previous")}
+//             onSelect={handleSelect}>
         
-            <Carousel.Item>
-              <p>PERSONALIZE YOUR EXPERIENCE</p>
-              <h3>What are your favorite cuisines?</h3>
-              <Carousel.Caption>
-                {/* <h3></h3>
-                <p></p> */}
-              </Carousel.Caption>
-            </Carousel.Item>
+//             <Carousel.Item>
+//               <p>PERSONALIZE YOUR EXPERIENCE</p>
+//               <h3>What are your favorite cuisines?</h3>
+//               <Carousel.Caption>
+//                 {/* <h3></h3>
+//                 <p></p> */}
+//               </Carousel.Caption>
+//             </Carousel.Item>
 
-            <Carousel.Item>
-              <p>PERSONALIZE YOUR EXPERIENCE</p>
-              <h3>Do you follow any of these diets?</h3>
-              <Carousel.Caption>
-                {/* <h3></h3>
-                <p></p> */}
-              </Carousel.Caption>
-            </Carousel.Item>
+//             <Carousel.Item>
+//               <p>PERSONALIZE YOUR EXPERIENCE</p>
+//               <h3>Do you follow any of these diets?</h3>
+//               <Carousel.Caption>
+//                 {/* <h3></h3>
+//                 <p></p> */}
+//               </Carousel.Caption>
+//             </Carousel.Item>
 
-            <Carousel.Item>
-              <p>PERSONALIZE YOUR EXPERIENCE</p>
-              <h3>How would you describe your cooking skills?</h3>
-              <Carousel.Caption>
-                {/* <h3></h3>
-                <p></p> */}
-              </Carousel.Caption>
-            </Carousel.Item>
+//             <Carousel.Item>
+//               <p>PERSONALIZE YOUR EXPERIENCE</p>
+//               <h3>How would you describe your cooking skills?</h3>
+//               <Carousel.Caption>
+//                 {/* <h3></h3>
+//                 <p></p> */}
+//               </Carousel.Caption>
+//             </Carousel.Item>
             
-            <Carousel.Item>
-              <h3>Create your account by filling out the information below</h3>
-            <Carousel.Caption>
-              <MultiFormSignUp />
-            </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </>
-    )
-}
+//             <Carousel.Item>
+//               <h3>Create your account by filling out the information below</h3>
+//             <Carousel.Caption>
+//               <MultiFormSignUp />
+//             </Carousel.Caption>
+//             </Carousel.Item>
+//           </Carousel>
+//         </>
+//     )
+// }
 
-export default RegistrationCarousel;
+// export default RegistrationCarousel;
