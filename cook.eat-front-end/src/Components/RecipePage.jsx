a4c432ab96cfb58bf3c8d49d57d9ad8979cca2a9
yuval-mandler <yuvalmandler@gmail.com>
2021-01-25 17:27:57 +0200

    cuisine

--- cook.eat-front-end/src/Components/RecipePage.jsx
+++ cook.eat-front-end/src/Components/RecipePage.jsx
@@ -1,110 +1,17 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import '../styles/recipePage.css';
-import { FaPen, FaHeart, FaFireAlt, FaClock } from 'react-icons/fa';
-import { GiCookingPot } from 'react-icons/gi';
-import { useAuth } from '../Conteaxts/autoConteaxt';
-=======
 import React, { useState, useEffect } from 'react'
 import '../styles/recipePage.css'
 import { FaPen, FaHeart, FaFireAlt, FaClock, FaPizzaSlice } from 'react-icons/fa'
 import { GiCookingPot } from 'react-icons/gi'
 import { loadRecipe, likeRecipe } from '../libs/utils'
-<<<<<<< HEAD
-import { useAuth } from '../Conteaxts/autoConteaxt'
-=======
->>>>>>> f1a33767b7065046abb7c4721adee1e0b914982b
->>>>>>> adf2f9e2d4ee72a49515c308ecdddb6ac77a29bd
+import { cuisinePic } from '../libs/cuisine'
 
 function RecipePage(props) {
-	const { currentUser } = useAuth;
 
-<<<<<<< HEAD
-	console.log(currentUser);
-	const BASE_URL = 'http://localhost:5000';
-	const [recipe, setRecipe] = useState();
-	// const {id} = props
-	const id = '600d3dd109c0130810aa4560';
-	const getRecipe = async () => {
-		const response = await fetch(`${BASE_URL}/recipes/${id}`);
-		const data = await response.json();
-		console.log(data);
-		setRecipe(data);
-	};
-
-	useEffect(() => {
-		getRecipe();
-	}, []);
-
-	return (
-		<div>
-			{recipe ? (
-				<div className='recipePage'>
-					<div className='titleBox'>
-						<div>
-							<h1 className='recipeTitle'>{recipe.recipeTitle}</h1>
-							<h4 style={{ fontSize: '14px' }}>
-								<FaPen style={{ marginRight: '1rem' }} />
-								{recipe.writer}
-							</h4>
-							<div className='likeBox'>
-								{' '}
-								<FaHeart style={{ marginRight: '1rem' }} />
-								{recipe.likes}
-							</div>
-							<div className='desc'>{recipe.description}</div>
-							<div className='topic'>
-								<div className='cal'>
-									{' '}
-									<FaFireAlt style={{ marginRight: '1rem' }} />
-									{recipe.calories} Cal.
-								</div>
-								<div className='cal'>
-									{' '}
-									<GiCookingPot style={{ marginRight: '1rem', fontSize: '30px' }} />
-									{recipe.dishLevel}{' '}
-								</div>
-								<div className='cal clock'>
-									{' '}
-									<FaClock style={{ marginRight: '1rem' }} /> {recipe.preparationTime} Min.
-								</div>
-							</div>
-						</div>
-						<div className='imageBox'>
-							<img className='recipeImage' src={recipe.picture} alt='pasta' />
-						</div>
-					</div>
-					<h3>{recipe.cuisineType}</h3>
-					<div className='textBox'>
-						<div className='textLeft'>
-							<h3>How to make it.</h3>
-							<ol>
-								{recipe.steps.map((step) => (
-									<li className='step'>{step}</li>
-								))}
-							</ol>
-						</div>
-						<div className='textRight'>
-							<h3>Ingredients </h3>
-							{recipe.servings && <h5>{recipe.servings} servings</h5>}
-							{recipe.ingredients.map((ing) => (
-								<div className='ing'>
-									<span>{ing.ingredientName}</span>
-									<span>{ing.quantity}</span>
-									<span>{ing.units}</span>
-								</div>
-							))}
-						</div>
-					</div>
-				</div>
-			) : null}
-		</div>
-	);
-=======
     const [ recipe, setRecipe ] = useState()
     const [ countLikes, setCountLikes ] = useState()
     const [ like, setLike ] = useState() 
-    const  { currentUser } = useAuth
+    const [ user, setUser ] = useState()
+    const [ cuisines, setCuisines ] = useState()
     // const {id} = props
     const id = '600d3dd109c0130810aa4560'
 
@@ -125,13 +32,15 @@ function RecipePage(props) {
     const getRecipe = async() => {
         const result = await loadRecipe(id)
         await setRecipe(result)
-        recipe && setCountLikes(recipe.likes)
-        
+        if(recipe){
+            setCountLikes(recipe.likes)
+            const response = cuisinePic(recipe.cuisineType)
+            setCuisines(response)
+        }    
     }
 
     useEffect(() => {
         getRecipe()
-        console.log(currentUser)
     }, [countLikes])
 
 
@@ -142,8 +51,13 @@ function RecipePage(props) {
         <div>
             <h1 className='recipeTitle'>{recipe.recipeTitle}</h1>
             <h4 style={{fontSize: '14px'}}><FaPen style={{marginRight:'1rem'}}/>{recipe.writer}</h4> 
-            <button type='click' onClick={addLike} className='likeBox'><FaHeart style={{marginRight:'1rem'}} />{recipe.likes}</button>  
-            <h3><FaPizzaSlice/>{recipe.cuisineType}</h3>  
+            <div className='cuisineAndLike'>
+                <div className='cuisineBox'>
+                    <img className='cuisineImg' src={cuisines} alt={recipe.cuisineType}/>
+                    <div>{recipe.cuisineType}</div>  
+                </div>
+                <button type='click' onClick={addLike} className='likeBox'><FaHeart style={{marginRight:'1rem'}} />{recipe.likes}</button>  
+            </div>
             <div className='desc'>{recipe.description}</div>
             <div className='topic'>
                 <div className='cal clock'> <FaFireAlt style={{marginRight:'1rem', fontSize: '25px',color:'crimson'}}/>{recipe.calories} Cal.</div>
@@ -179,7 +93,6 @@ function RecipePage(props) {
         </div>: null}
         </div>
     )
->>>>>>> f1a33767b7065046abb7c4721adee1e0b914982b
 }
 
-export default RecipePage;
+export default RecipePage
