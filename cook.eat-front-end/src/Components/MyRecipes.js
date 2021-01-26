a1ff5a9d17fe3cb52886a38af60b8d29c1abbbee
isayas <gadlo323@gmail.com>
2021-01-26 19:56:28 +0200

    fix: my recipes

--- cook.eat-front-end/src/Components/MyRecipes.js
+++ cook.eat-front-end/src/Components/MyRecipes.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/MyRecipes.css";
 import mockRecipes from "../mockdata/mockRecipes";
 import MyRecipesCard from "./MyRecipesCard";
+import { useAuth } from "../Conteaxts/autoConteaxt";
 const MyRecipes = () => {
+  const { myRecipes } = useAuth();
+  const [Recipes, setRecipes] = useState([]);
+
+  const owensRecipes = async () => {
+    const obj = await myRecipes();
+    setRecipes(obj);
+  };
+  useEffect(() => {
+    owensRecipes();
+  }, []);
   return (
     <div className="my-recipes-pagewrapper">
       {/* <h4>My Recipes</h4> */}
       <div className="conteiner-cards-recipe">
-        {mockRecipes.map(function (recipe, i) {
+        {Recipes.map((recipe, i) => {
           return <MyRecipesCard key={i} recipe={recipe} />;
         })}
       </div>
