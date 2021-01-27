a4139f8b4f22f7d33a2ca86ce5c0fcaa04219944
isayas <gadlo323@gmail.com>
2021-01-27 19:25:26 +0200

    fix:add recipe

--- cook.eat-front-end/src/Components/MyRecipesCard.js
+++ cook.eat-front-end/src/Components/MyRecipesCard.js
@@ -13,10 +13,20 @@ import { FaHeart } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
 import { BiTime } from "react-icons/bi";
 import { GiCookingPot } from "react-icons/gi";
+import { BounceLoader } from "react-spinners";
+import { css } from "@emotion/react";
+import { useAuth } from "../Conteaxts/autoConteaxt";
 
+const override = css`
+  position: fixed;
+  top: 40%;
+  left: 40%;
+  border-color: red;
+`;
 const MyRecipesCard = (props) => {
   const [user, setUser] = useState();
-
+  const { removeRecipe } = useAuth();
+  const [loading, setLoading] = useState(false);
   const { recipe } = props;
 
   const getUser = () => {
@@ -29,6 +39,14 @@ const MyRecipesCard = (props) => {
   useEffect(() => {
     getUser();
   }, []);
+
+  const deleteRecipe = async () => {
+    setLoading(true);
+    // setDisabled(false);
+    const res = await removeRecipe(recipe._id);
+    setLoading(false);
+    if (res) window.location.reload();
+  };
 
   return (
     <div className="my-recipe-cards-container">
@@ -75,6 +93,11 @@ const MyRecipesCard = (props) => {
             >
               <b>Show More</b>
             </NavLink>
+            {user === recipe.writer && (
+              <button className="show-more-card" onClick={deleteRecipe}>
+                <b>Delete</b>
+              </button>
+            )}
             {/* {user === recipe.writer && (
               <NavLink
                 exact
@@ -87,6 +110,12 @@ const MyRecipesCard = (props) => {
           </div>
         </Card.Body>
       </Card>
+      <BounceLoader
+        css={override}
+        size={80}
+        color={"#B73032"}
+        loading={loading}
+      />
     </div>
   );
 };
